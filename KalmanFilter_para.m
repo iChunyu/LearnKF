080e774ef6ada7af51aa783837fd613971649f75
Chunyu <chunyu2018@foxmail.com>
2019-10-03 11:28:40 +0800

    make up simulation

--- KalmanFilter_para.m
+++ KalmanFilter_para.m
@@ -1,8 +1,43 @@
-Ts = 0.001;
-A = [1 1; 0 1];
-B = [0 1]';
-C = [1 0];
-D = 0;
+% Build my Kalman filter and test it
 
-Q = diag([1e-3 2e-3]);
-R = 0.1;
+% XiaoCY 2019-10-02
+
+%% Initialization
+set(0,'DefaultLineLineWidth',2)
+set(0,'DefaultAxesFontSize',20)
+set(0,'DefaultFigureWindowStyle','docked')
+set(0,'DefaultFigureColor','w')
+
+clear;clc
+close all
+
+%%
+Ts = 1e-3;
+
+omg0 = 2*pi/1.5;
+ksi = 0.5;
+m = 1;
+
+Ac = [0 1; -omg0^2 -ksi];
+Bc = [ 0 1/m]';
+Cc = [ 1 0];
+IniC = [ 1 0];
+
+Q = 1e-3;
+R = 1e-3;
+
+Ad = Ac*Ts+eye(2);
+Bd = Bc*Ts;
+Cd = Cc;
+PredC = [0.8 0];
+
+%%
+sim('KalmanFilterSim')
+figure('Name','Result')
+plot(t,xm)
+hold on
+grid on
+plot(t,xp)
+xlabel('Time (s)')
+ylabel('Position (m)')
+legend('Measurement','Filtered')
